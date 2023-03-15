50e182aa7c322f3846f8807e9481f7c19b9cfad9
sdsarun <sdsarun@outlook.com>
2023-03-15 17:50:32 +0700

    done task-3

---
+++ solve3.js
@@ -0,0 +1,28 @@
+// Part - 1
+let x = 203;
+while (x >= 0) {
+    x -= 0.5;
+    console.log(x);
+}
+console.log(x);
+
+// Part - 2
+for (let i = 1; i <= 100; i++) {
+    let isOdd = i % 2 != 0;
+    if (isOdd) {
+        console.log(i);
+    }
+}
+
+// Part - 3
+const sumNth = (n) => {
+    let sum = 0, start = 1;
+    while (start <= n) {
+        sum += start;
+        start++;
+    }
+    return sum;
+}
+
+console.log(sumNth(5));
+console.log(sumNth(19));
